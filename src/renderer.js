aa2eadad8136a34a740476e9c93b7f8aa5706e34
demipel8 <demipel8@gmail.com>
2015-07-31 16:23:03 +0200

    rendered docs updated

--- src/renderer.js
+++ src/renderer.js
@@ -4,17 +4,18 @@
 
 var CraftyRenderer = {};
 
-(function(Render) {
-    
+( function( Render ) {
+
     /**
-     * Creates a new renderer. The options parameter is an object that specifies any properties you wish to override the defaults.
-     * All properties have default values, and many are pre-calculated automatically based on other properties.
-     * See the properties section below for detailed information on what you can pass via the `options` object.
+     * Creates a new renderer. The options parameter is an object that specifies any properties you wish to override
+     * the defaults. All properties have default values, and many are pre-calculated automatically based on other
+     * properties. See the properties section below for detailed information on what you can pass via the `options`
+     * object.
      * @method create
      * @param {object} [options]
      * @return {render} A new renderer
      */
-    Render.create = function(options) {
+    Render.create = function( options ) {
         var defaults = {
             controller: Render,
             element: null,
@@ -43,20 +44,20 @@ var CraftyRenderer = {};
             }
         };
 
-        var render = Common.extend(defaults, options);
+        var render = Common.extend( defaults, options );
 
         render.textures = {};
 
-        render.bounds = render.bounds || { 
-            min: { 
-                x: 0,
-                y: 0
-            }, 
-            max: { 
-                x: render.options.width,
-                y: render.options.height
-            }
-        };
+        render.bounds = render.bounds || {
+                min: {
+                    x: 0,
+                    y: 0
+                },
+                max: {
+                    x: render.options.width,
+                    y: render.options.height
+                }
+            };
 
         return render;
     };
@@ -67,18 +68,14 @@ var CraftyRenderer = {};
      * @method world
      * @param {engine} engine
      */
-    Render.world = function(engine) {
+    Render.world = function( engine ) {
         var render = engine.render;
         var world = engine.world;
         var options = render.options;
-        var allBodies = Composite.allBodies(world);
-        var allConstraints = Composite.allConstraints(world);
+        var allBodies = Composite.allBodies( world );
+        var allConstraints = Composite.allConstraints( world );
         var bodies = [];
         var constraints = [];
-        var boundsWidth;
-        var boundsHeight;
-        var boundsScaleX;
-        var boundsScaleY;
         var body;
         var constraint;
         var bodyA;
@@ -87,32 +84,28 @@ var CraftyRenderer = {};
         var pointBWorld;
         var i;
 
-        // handle bounds
+        // Handle bounds
         if ( options.hasBounds ) {
-            boundsWidth = render.bounds.max.x - render.bounds.min.x;
-            boundsHeight = render.bounds.max.y - render.bounds.min.y;
-            boundsScaleX = boundsWidth / options.width;
-            boundsScaleY = boundsHeight / options.height;
 
-            // filter out bodies that are not in view
+            // Filter out bodies that are not in view
             for ( i = 0; i < allBodies.length; i++ ) {
-                body = allBodies[i];
-                if ( Bounds.overlaps( body.bounds, render.bounds ) ){
-                    bodies.push(body);
+                body = allBodies[ i ];
+                if ( Bounds.overlaps( body.bounds, render.bounds ) ) {
+                    bodies.push( body );
                 }
             }
 
-            // filter out constraints that are not in view
+            // Filter out constraints that are not in view
             for ( i = 0; i < allConstraints.length; i++ ) {
-                constraint = allConstraints[i];
+                constraint = allConstraints[ i ];
                 bodyA = constraint.bodyA;
                 bodyB = constraint.bodyB;
                 pointAWorld = constraint.pointA;
                 pointBWorld = constraint.pointB;
 
                 if ( bodyA ) {
-                   pointAWorld = Vector.add( bodyA.position, constraint.pointA ); 
-                } 
+                    pointAWorld = Vector.add( bodyA.position, constraint.pointA );
+                }
 
                 if ( bodyB ) {
                     pointBWorld = Vector.add( bodyB.position, constraint.pointB );
@@ -132,16 +125,16 @@ var CraftyRenderer = {};
             bodies = allBodies;
         }
 
-        //move bodies
+        // Move bodies
         Render.bodies( engine, bodies );
 
         //Only show constraints when debug is active
-        if( debug.debugAllowed ) {
+        if ( debug.debugAllowed ) {
             Render.constraints( constraints );
         }
 
-        if (options.showDebug) {
-            Render.debug(engine);
+        if ( options.showDebug ) {
+            Render.debug( engine );
         }
     };
 
@@ -150,64 +143,64 @@ var CraftyRenderer = {};
      * @private
      * @method debug
      * @param {engine} engine
-     * @param {RenderingContext} context
      */
     Render.debug = function( engine ) {
-        var canvas = _createCanvas( engine.world.bounds.max.x, engine.world.bounds.max.y);
+        var canvas = _createCanvas( engine.world.bounds.max.x, engine.world.bounds.max.y );
         canvas.width = engine.world.bounds.max.x;
         canvas.height = engine.world.bounds.max.y;
         canvas.style.zIndex   = 8;
-        canvas.style.position = "absolute";
+        canvas.style.position = 'absolute';
 
-        var c = canvas.getContext('2d');
+        var c = canvas.getContext( '2d' );
         var world = engine.world;
         var render = engine.render;
         var options = render.options;
-        var bodies = Composite.allBodies(world);
-        var space = "    ";
+        var bodies = Composite.allBodies( world );
+        var space = '    ';
 
-        document.body.appendChild(canvas);
+        document.body.appendChild( canvas );
 
-        if (engine.timing.timestamp - (render.debugTimestamp || 0) >= 500) {
-            var text = "";
-            text += "fps: " + Math.round(engine.timing.fps) + space;
+        if ( engine.timing.timestamp - ( render.debugTimestamp || 0 ) >= 500 ) {
+            var text = '';
+            text += 'fps: ' + Math.round( engine.timing.fps ) + space;
 
             // @if DEBUG
-            if (engine.metrics.extended) {
-                text += "delta: " + engine.timing.delta.toFixed(3) + space;
-                text += "correction: " + engine.timing.correction.toFixed(3) + space;
-                text += "bodies: " + bodies.length + space;
+            if ( engine.metrics.extended ) {
+                text += 'delta: ' + engine.timing.delta.toFixed( 3 ) + space;
+                text += 'correction: ' + engine.timing.correction.toFixed( 3 ) + space;
+                text += 'bodies: ' + bodies.length + space;
 
-                if (engine.broadphase.controller === Grid)
-                    text += "buckets: " + engine.metrics.buckets + space;
+                if ( engine.broadphase.controller === Grid ) {
+                    text += 'buckets: ' + engine.metrics.buckets + space;
+                }
 
-                text += "\n";
+                text += '\n';
 
-                text += "collisions: " + engine.metrics.collisions + space;
-                text += "pairs: " + engine.pairs.list.length + space;
-                text += "broad: " + engine.metrics.broadEff + space;
-                text += "mid: " + engine.metrics.midEff + space;
-                text += "narrow: " + engine.metrics.narrowEff + space;
+                text += 'collisions: ' + engine.metrics.collisions + space;
+                text += 'pairs: ' + engine.pairs.list.length + space;
+                text += 'broad: ' + engine.metrics.broadEff + space;
+                text += 'mid: ' + engine.metrics.midEff + space;
+                text += 'narrow: ' + engine.metrics.narrowEff + space;
             }
-            // @endif            
 
+            // @endif
             render.debugString = text;
             render.debugTimestamp = engine.timing.timestamp;
         }
 
-        if (render.debugString) {
-            c.font = "12px Arial";
+        if ( render.debugString ) {
+            c.font = '12px Arial';
 
-            if (options.wireframes) {
+            if ( options.wireframes ) {
                 c.fillStyle = 'rgba(255,255,255,0.5)';
             } else {
                 c.fillStyle = 'rgba(0,0,0,0.5)';
             }
 
-            var split = render.debugString.split('\n');
+            var split = render.debugString.split( '\n' );
 
-            for (var i = 0; i < split.length; i++) {
-                c.fillText(split[i], 50, 50 + i * 18);
+            for ( var i = 0; i < split.length; i++ ) {
+                c.fillText( split[ i ], 50, 50 + i * 18 );
             }
         }
     };
@@ -217,7 +210,6 @@ var CraftyRenderer = {};
      * @private
      * @method constraints
      * @param {constraint[]} constraints
-     * @param {RenderingContext} context
      */
     Render.constraints = function( constraints ) {
         var constraint;
@@ -226,22 +218,21 @@ var CraftyRenderer = {};
         var pointToGo;
 
         for ( var i = 0; i < constraints.length; i++ ) {
-            constraint = constraints[i];
+            constraint = constraints[ i ];
 
             if ( !constraint.render.visible || !constraint.pointA || !constraint.pointB ) {
                 continue;
             }
 
             if ( !constraint.entity ) {
-               constraint.entity = Crafty.e( RenderingMode + ', Color')
-                .color( 'white' );
-            } 
+                constraint.entity = Crafty.e( RenderingMode + ', Color' )
+                    .color( 'white' );
+            }
 
             constraint.entity.h = Crafty.viewport.height * 0.005;
 
             bodyA = constraint.bodyA;
             bodyB = constraint.bodyB;
-
 
             if ( bodyA ) {
                 constraint.entity.x = bodyA.position.x + constraint.pointA.x;
@@ -253,28 +244,28 @@ var CraftyRenderer = {};
 
             if ( bodyB ) {
                 pointToGo = {
-                    x : bodyB.position.x + constraint.pointB.x,
-                    y : bodyB.position.y + constraint.pointB.y
+                    x: bodyB.position.x + constraint.pointB.x,
+                    y: bodyB.position.y + constraint.pointB.y
                 };
             } else {
                 pointToGo = {
-                    x : constraint.pointB.x,
-                    y : constraint.pointB.y
+                    x: constraint.pointB.x,
+                    y: constraint.pointB.y
                 };
             }
 
             constraint.entity.w = _getWidth( {
-                x : constraint.entity._x,
-                y : constraint.entity._y
+                x: constraint.entity._x,
+                y: constraint.entity._y
             }, pointToGo );
 
             constraint.entity.rotation = _getAngle( {
-                x : constraint.entity._x,
-                y : constraint.entity._y
+                x: constraint.entity._x,
+                y: constraint.entity._y
             }, pointToGo );
         }
     };
-    
+
     /**
      * Description
      * @private
@@ -282,7 +273,7 @@ var CraftyRenderer = {};
      * @param {engine} engine
      * @param {body[]} bodies
      */
-    Render.bodies = function(engine, bodies) {
+    Render.bodies = function( engine, bodies ) {
         var render = engine.render;
         var options = render.options;
         var body;
@@ -291,7 +282,7 @@ var CraftyRenderer = {};
         var k;
 
         for ( i = 0; i < bodies.length; i++ ) {
-            body = bodies[i];
+            body = bodies[ i ];
             entity = body.entity;
 
             if ( !body.render.visible ) {
@@ -299,45 +290,45 @@ var CraftyRenderer = {};
             }
 
             if ( !body.parts ) {
-                _moveEntity(options, body, body.isSleeping); 
+                _moveEntity( options, body, body.isSleeping );
             } else {
-                // handle compound parts
+
+                // Handle compound parts
                 for ( k = body.parts.length > 1 ? 1 : 0; k < body.parts.length; k++ ) {
-                    _moveEntity(options, body.parts[k], body.isSleeping);                
+                    _moveEntity( options, body.parts[ k ], body.isSleeping );
                 }
             }
         }
     };
 
-
     /**
      * Description
      * @method _createCanvas
      * @private
-     * @param {} width
-     * @param {} height
+     * @param {number} width
+     * @param {number} height
      * @return canvas
      */
     var _createCanvas = function( width, height ) {
-        var canvas = document.createElement('canvas');
+        var canvas = document.createElement( 'canvas' );
         canvas.width = width;
         canvas.height = height;
 
-        canvas.oncontextmenu = function() { 
-            return false; 
+        canvas.oncontextmenu = function() {
+            return false;
         };
-        canvas.onselectstart = function() { 
-            return false; 
+        canvas.onselectstart = function() {
+            return false;
         };
 
-        return canvas; 
+        return canvas;
     };
 
     /**
      * Moves an entity according to matter calculations
      * @param  {object}  options    engine.options
      * @param  {object}  part       part to be moved
-     * @param  {Boolean} isSleeping 
+     * @param  {Boolean} isSleeping
      */
     var _moveEntity = function( options, part, isSleeping ) {
         var entity = part.entity;
@@ -346,16 +337,15 @@ var CraftyRenderer = {};
             entity.alpha = 0.5;
         }
 
-        if( entity._x !== part.position.x - ( entity._w / 2 ) ) {
+        if ( entity._x !== part.position.x - ( entity._w / 2 ) ) {
             entity.matterMoved = true;
             entity.x = part.position.x - ( entity._w / 2 );
         }
 
-        if( entity._y !== part.position.y - ( entity._h / 2 ) ) {
+        if ( entity._y !== part.position.y - ( entity._h / 2 ) ) {
             entity.matterMoved = true;
             entity.y = part.position.y - ( entity._h / 2 );
         }
-        
 
         debug.moveEntity( entity );
 
@@ -363,25 +353,25 @@ var CraftyRenderer = {};
 
     };
 
-    //initial support only for center origin
+    // Initial support only for center origin
     var _rotateEntity = function( entity, angle ) {
 
         var angleFixed = Crafty.math.radToDeg( angle ).toFixed( 3 );
-        
-        if( angle === 0 || entity._rotation === angleFixed ) {
+
+        if ( angle === 0 || entity._rotation === angleFixed ) {
             return;
         }
-        
+
         entity.matterMoved = true;
         entity.rotation = angleFixed;
 
-        debug.rotateEntity( [ entity, angleFixed] );
+        debug.rotateEntity( [ entity, angleFixed ] );
     };
 
     /**
      * Calculate the distance between two points
-     * @param  {Vector} pointA 
-     * @param  {Vector} pointB 
+     * @param  {Vector} pointA
+     * @param  {Vector} pointB
      * @return {number}        distance between two points
      */
     var _getWidth = function( pointA, pointB ) {
@@ -399,7 +389,7 @@ var CraftyRenderer = {};
     var _getAngle = function( pointA, pointB ) {
 
         var vector = _getVector( pointA, pointB );
-        return - Crafty.math.radToDeg( Math.atan2( vector.y, vector.x ) ).toFixed( 3 );
+        return -Crafty.math.radToDeg( Math.atan2( vector.y, vector.x ) ).toFixed( 3 );
     };
 
     /**
@@ -410,7 +400,7 @@ var CraftyRenderer = {};
      */
     var _getVector = function( pointA, pointB ) {
 
-        return Vector.create( pointB.x - pointA.x, - ( pointB.y - pointA.y ) );
+        return Vector.create( pointB.x - pointA.x, -( pointB.y - pointA.y ) );
     };
 
-})(CraftyRenderer);
+} )( CraftyRenderer );
